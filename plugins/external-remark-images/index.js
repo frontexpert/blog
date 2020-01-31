c8eeea57daa7e1df67b98e1ca2b90a5eea1863f7
Super Appdev <frontexport1987@gmail.com>
2020-01-31 19:33:17 -0300

    lint

--- plugins/external-remark-images/index.js
+++ plugins/external-remark-images/index.js
@@ -2,19 +2,19 @@ const {
   DEFAULT_OPTIONS,
   imageClass,
   imageBackgroundClass,
-  imageWrapperClass,
-} = require(`./constants`)
-const visitWithParents = require(`unist-util-visit-parents`)
-const getDefinitions = require(`mdast-util-definitions`)
-const path = require(`path`)
-const queryString = require(`query-string`)
-const isRelativeUrl = require(`is-relative-url`)
-const _ = require(`lodash`)
-const { fluid, stats, traceSVG } = require(`gatsby-plugin-sharp`)
-const Promise = require(`bluebird`)
-const cheerio = require(`cheerio`)
-const { slash } = require(`gatsby-core-utils`)
-const chalk = require(`chalk`)
+  imageWrapperClass
+} = require(`./constants`);
+const visitWithParents = require(`unist-util-visit-parents`);
+const getDefinitions = require(`mdast-util-definitions`);
+const path = require(`path`);
+const queryString = require(`query-string`);
+const isRelativeUrl = require(`is-relative-url`);
+const _ = require(`lodash`);
+const { fluid, stats, traceSVG } = require(`gatsby-plugin-sharp`);
+const Promise = require(`bluebird`);
+const cheerio = require(`cheerio`);
+const { slash } = require(`gatsby-core-utils`);
+const chalk = require(`chalk`);
 
 // If the image is relative (not hosted elsewhere)
 // 1. Find the image file
@@ -31,99 +31,99 @@ module.exports = (
     getNode,
     reporter,
     cache,
-    compiler,
+    compiler
   },
   pluginOptions
 ) => {
-  const options = _.defaults(pluginOptions, { pathPrefix }, DEFAULT_OPTIONS)
+  const options = _.defaults(pluginOptions, { pathPrefix }, DEFAULT_OPTIONS);
 
   const findParentLinks = ({ children }) =>
     children.some(
       node =>
         (node.type === `html` && !!node.value.match(/<a /)) ||
         node.type === `link`
-    )
+    );
 
   // Get all the available definitions in the markdown tree
-  const definitions = getDefinitions(markdownAST)
+  const definitions = getDefinitions(markdownAST);
 
   // This will allow the use of html image tags
   // const rawHtmlNodes = select(markdownAST, `html`)
-  let rawHtmlNodes = []
+  let rawHtmlNodes = [];
   visitWithParents(markdownAST, [`html`, `jsx`], (node, ancestors) => {
-    const inLink = ancestors.some(findParentLinks)
+    const inLink = ancestors.some(findParentLinks);
 
-    rawHtmlNodes.push({ node, inLink })
-  })
+    rawHtmlNodes.push({ node, inLink });
+  });
 
   // This will only work for markdown syntax image tags
-  let markdownImageNodes = []
+  let markdownImageNodes = [];
 
   visitWithParents(
     markdownAST,
     [`image`, `imageReference`],
     (node, ancestors) => {
-      const inLink = ancestors.some(findParentLinks)
+      const inLink = ancestors.some(findParentLinks);
 
-      markdownImageNodes.push({ node, inLink })
+      markdownImageNodes.push({ node, inLink });
     }
-  )
+  );
 
   const getImageInfo = uri => {
-    const { url, query } = queryString.parseUrl(uri)
+    const { url, query } = queryString.parseUrl(uri);
     return {
       ext: path
         .extname(url)
         .split(`.`)
         .pop(),
       url,
-      query,
-    }
-  }
+      query
+    };
+  };
 
   const getImageCaption = (node, overWrites) => {
     const getCaptionString = () => {
       const captionOptions = Array.isArray(options.showCaptions)
         ? options.showCaptions
         : options.showCaptions === true
-          ? [`title`, `alt`]
-          : false
+        ? [`title`, `alt`]
+        : false;
 
       if (captionOptions) {
         for (const option of captionOptions) {
           switch (option) {
             case `title`:
               if (node.title) {
-                return node.title
+                return node.title;
               }
-              break
+              break;
             case `alt`:
               if (overWrites.alt) {
-                return overWrites.alt
+                return overWrites.alt;
               }
               if (node.alt) {
-                return node.alt
+                return node.alt;
               }
-              break
+              break;
           }
         }
       }
 
-      return ``
-    }
+      return ``;
+    };
 
-    const captionString = getCaptionString()
+    const captionString = getCaptionString();
 
     if (!options.markdownCaptions || !compiler) {
-      return _.escape(captionString)
+      return _.escape(captionString);
     }
 
-    return compiler.generateHTML(compiler.parseString(captionString))
-  }
+    return compiler.generateHTML(compiler.parseString(captionString));
+  };
 
   // Takes a node and generates the needed images and then returns
   // the needed HTML replacement for the image
-  const generateImagesAndUpdateNode = async function (
+  const generateImagesAndUpdateNode = async function(
     node,
     resolve,
     inLink,
@@ -131,71 +131,74 @@ module.exports = (
   ) {
     // Check if this markdownNode has a File parent. This plugin
     // won't work if the image isn't hosted locally.
-    const parentNode = getNode(markdownNode.parent)
-    let imagePath
+    const parentNode = getNode(markdownNode.parent);
+    let imagePath;
     if (parentNode && parentNode.dir) {
-      imagePath = slash(path.join(parentNode.dir, getImageInfo(node.url).url))
+      imagePath = slash(path.join(parentNode.dir, getImageInfo(node.url).url));
     } else {
-      return null
+      return null;
     }
 
     const imageNode = _.find(files, file => {
       if (file && file.absolutePath) {
-        return file.absolutePath === imagePath
+        return file.absolutePath === imagePath;
       }
-      return null
-    })
+      return null;
+    });
 
     if (!imageNode || !imageNode.absolutePath) {
-      return resolve()
+      return resolve();
     }
 
     let fluidResult = await fluid({
       file: imageNode,
       args: options,
       reporter,
-      cache,
-    })
+      cache
+    });
 
     if (!fluidResult) {
-      return resolve()
+      return resolve();
     }
 
-    const originalImg = fluidResult.originalImg
-    const fallbackSrc = fluidResult.src
-    const srcSet = fluidResult.srcSet
-    const presentationWidth = fluidResult.presentationWidth
+    const originalImg = fluidResult.originalImg;
+    const fallbackSrc = fluidResult.src;
+    const srcSet = fluidResult.srcSet;
+    const presentationWidth = fluidResult.presentationWidth;
 
     // Generate default alt tag
-    const srcSplit = getImageInfo(node.url).url.split(`/`)
-    const fileName = srcSplit[srcSplit.length - 1]
-    const fileNameNoExt = fileName.replace(/\.[^/.]+$/, ``)
-    const defaultAlt = fileNameNoExt.replace(/[^A-Z0-9]/gi, ` `)
+    const srcSplit = getImageInfo(node.url).url.split(`/`);
+    const fileName = srcSplit[srcSplit.length - 1];
+    const fileNameNoExt = fileName.replace(/\.[^/.]+$/, ``);
+    const defaultAlt = fileNameNoExt.replace(/[^A-Z0-9]/gi, ` `);
 
     const alt = _.escape(
       overWrites.alt ? overWrites.alt : node.alt ? node.alt : defaultAlt
-    )
+    );
 
-    const nodeTitle = node.title ? _.escape(node.title) : alt
+    const nodeTitle = node.title ? _.escape(node.title) : alt;
 
-    let title = nodeTitle.trim().replace(/=\d{2,4}x\d{2,4}|=\d{2,4}/gi, ' ').trim()
+    let title = nodeTitle
+      .trim()
+      .replace(/=\d{2,4}x\d{2,4}|=\d{2,4}/gi, ' ')
+      .trim();
 
     if (!title) {
-      title = alt
+      title = alt;
     }
 
-    const resize = nodeTitle.trim().match(/=\d{2,4}x\d{2,4}|=\d{2,4}/gi)
+    const resize = nodeTitle.trim().match(/=\d{2,4}x\d{2,4}|=\d{2,4}/gi);
 
     let width = null;
     let height = null;
 
     if (resize) {
-      const [w, h] = resize[0].replace(/^(.)/g, '').split('x')
+      const [w, h] = resize[0].replace(/^(.)/g, '').split('x');
       width = w;
-      height = h
+      height = h;
     }
 
-    const loading = options.loading
+    const loading = options.loading;
 
     if (![`lazy`, `eager`, `auto`].includes(loading)) {
       reporter.warn(
@@ -204,7 +207,7 @@ module.exports = (
           `loading`
         )} option. Please pass one of "lazy", "eager" or "auto".
       `)
-      )
+      );
     }
 
     const imageStyle = `
@@ -212,7 +215,7 @@ module.exports = (
       height: ${height ? height + 'px' : '100%'};
       margin: 0;
       vertical-align: middle;
-     `.replace(/\s*(\S+:)\s*/g, `$1`)
+     `.replace(/\s*(\S+:)\s*/g, `$1`);
 
     // Create our base image tag
     let imageTag = `
@@ -226,7 +229,7 @@ module.exports = (
         style="${imageStyle}"
         loading="${loading}"
       />
-    `.trim()
+    `.trim();
 
     // if options.withWebp is enabled, generate a webp version and change the image tag to a picture tag
     if (options.withWebp) {
@@ -239,11 +242,11 @@ module.exports = (
           pluginOptions,
           DEFAULT_OPTIONS
         ),
-        reporter,
-      })
+        reporter
+      });
 
       if (!webpFluidResult) {
-        return resolve()
+        return resolve();
       }
 
       imageTag = `
@@ -267,64 +270,66 @@ module.exports = (
           style="${imageStyle}"
         />
       </picture>
-      `.trim()
+      `.trim();
     }
 
-    let placeholderImageData = fluidResult.base64
+    let placeholderImageData = fluidResult.base64;
 
     // if options.tracedSVG is enabled generate the traced SVG and use that as the placeholder image
     if (options.tracedSVG) {
-      let args = typeof options.tracedSVG === `object` ? options.tracedSVG : {}
+      let args = typeof options.tracedSVG === `object` ? options.tracedSVG : {};
 
       // Translate Potrace constants (e.g. TURNPOLICY_LEFT, COLOR_AUTO) to the values Potrace expects
-      const { Potrace } = require(`potrace`)
-      const argsKeys = Object.keys(args)
+      const { Potrace } = require(`potrace`);
+      const argsKeys = Object.keys(args);
       args = argsKeys.reduce((result, key) => {
-        const value = args[key]
-        result[key] = Potrace.hasOwnProperty(value) ? Potrace[value] : value
-        return result
-      }, {})
+        const value = args[key];
+        result[key] = Potrace.hasOwnProperty(value) ? Potrace[value] : value;
+        return result;
+      }, {});
 
       const tracedSVG = await traceSVG({
         file: imageNode,
         args,
         fileArgs: args,
         cache,
-        reporter,
-      })
+        reporter
+      });
 
       // Escape single quotes so the SVG data can be used in inline style attribute with single quotes
-      placeholderImageData = tracedSVG.replace(/'/g, `\\'`)
+      placeholderImageData = tracedSVG.replace(/'/g, `\\'`);
     }
 
-    let ratio = `${(1 / fluidResult.aspectRatio) * 100}%`
+    let ratio = `${(1 / fluidResult.aspectRatio) * 100}%`;
 
     if (width) {
-      const maxWidth = width < options.maxWidth ? width : options.maxWidth
-      ratio = height ? height + "px" : (1 / fluidResult.aspectRatio) * maxWidth + 'px'
+      const maxWidth = width < options.maxWidth ? width : options.maxWidth;
+      ratio = height
+        ? height + 'px'
+        : (1 / fluidResult.aspectRatio) * maxWidth + 'px';
     }
 
     const wrapperStyle =
       typeof options.wrapperStyle === `function`
         ? options.wrapperStyle(fluidResult)
-        : options.wrapperStyle
+        : options.wrapperStyle;
 
     // Construct new image node w/ aspect ratio placeholder
     const imageCaption =
-      options.showCaptions && getImageCaption(node, overWrites)
+      options.showCaptions && getImageCaption(node, overWrites);
 
-    let removeBgImage = false
+    let removeBgImage = false;
     if (options.disableBgImageOnAlpha) {
-      const imageStats = await stats({ file: imageNode, reporter })
-      if (imageStats && imageStats.isTransparent) removeBgImage = true
+      const imageStats = await stats({ file: imageNode, reporter });
+      if (imageStats && imageStats.isTransparent) removeBgImage = true;
     }
     if (options.disableBgImage) {
-      removeBgImage = true
+      removeBgImage = true;
     }
 
     const bgImage = removeBgImage
       ? ``
-      : ` background-image: url('${placeholderImageData}'); background-size: cover;`
+      : ` background-image: url('${placeholderImageData}'); background-size: cover;`;
 
     let rawHTML = `
   <span
@@ -332,7 +337,7 @@ module.exports = (
     style="width: ${width}px; padding-bottom: ${ratio}; position: relative; bottom: 0; left: 0;${bgImage} display: block;"
   ></span>
   ${imageTag}
-  `.trim()
+  `.trim();
 
     // Make linking to original image optional.
     if (!inLink && options.linkImagesToOriginal) {
@@ -346,19 +351,19 @@ module.exports = (
   >
     ${rawHTML}
   </a>
-    `.trim()
+    `.trim();
     }
 
     rawHTML = `
     <span
       class="${imageWrapperClass}"
       style="position: relative; display: block; margin-left: auto; margin-right: auto; ${
-      imageCaption ? `` : wrapperStyle
+        imageCaption ? `` : wrapperStyle
       } max-width: ${presentationWidth}px;"
     >
       ${rawHTML}
     </span>
-    `.trim()
+    `.trim();
 
     // Wrap in figure and use title as caption
     if (imageCaption) {
@@ -367,35 +372,35 @@ module.exports = (
     ${rawHTML}
     <figcaption class="gatsby-resp-image-figcaption">${imageCaption}</figcaption>
   </figure>
-      `.trim()
+      `.trim();
     }
 
-    return rawHTML
-  }
+    return rawHTML;
+  };
 
   return Promise.all(
     // Simple because there is no nesting in markdown
     markdownImageNodes.map(
       ({ node, inLink }) =>
         new Promise(async (resolve, reject) => {
-          const overWrites = {}
-          let refNode
+          const overWrites = {};
+          let refNode;
           if (
             !node.hasOwnProperty(`url`) &&
             node.hasOwnProperty(`identifier`)
           ) {
             //consider as imageReference node
-            refNode = node
-            node = definitions(refNode.identifier)
+            refNode = node;
+            node = definitions(refNode.identifier);
             // pass original alt from referencing node
-            overWrites.alt = refNode.alt
+            overWrites.alt = refNode.alt;
             if (!node) {
               // no definition found for image reference,
               // so there's nothing for us to do.
-              return resolve()
+              return resolve();
             }
           }
-          const fileType = getImageInfo(node.url).ext
+          const fileType = getImageInfo(node.url).ext;
 
           // Ignore gifs as we can't process them,
           // svgs as they are already responsive by definition
@@ -409,20 +414,20 @@ module.exports = (
               resolve,
               inLink,
               overWrites
-            )
+            );
 
             if (rawHTML) {
               // Replace the image or ref node with an inline HTML node.
               if (refNode) {
-                node = refNode
+                node = refNode;
               }
-              node.type = `html`
-              node.value = rawHTML
+              node.type = `html`;
+              node.value = rawHTML;
             }
-            return resolve(node)
+            return resolve(node);
           } else {
             // Image isn't relative so there's nothing for us to do.
-            return resolve()
+            return resolve();
           }
         })
     )
@@ -434,32 +439,32 @@ module.exports = (
         ({ node, inLink }) =>
           new Promise(async (resolve, reject) => {
             if (!node.value) {
-              return resolve()
+              return resolve();
             }
 
-            const $ = cheerio.load(node.value)
+            const $ = cheerio.load(node.value);
             if ($(`img`).length === 0) {
               // No img tags
-              return resolve()
+              return resolve();
             }
 
-            let imageRefs = []
-            $(`img`).each(function () {
-              imageRefs.push($(this))
-            })
+            let imageRefs = [];
+            $(`img`).each(function() {
+              imageRefs.push($(this));
+            });
 
             for (let thisImg of imageRefs) {
               // Get the details we need.
-              let formattedImgTag = {}
-              formattedImgTag.url = thisImg.attr(`src`)
-              formattedImgTag.title = thisImg.attr(`title`)
-              formattedImgTag.alt = thisImg.attr(`alt`)
+              let formattedImgTag = {};
+              formattedImgTag.url = thisImg.attr(`src`);
+              formattedImgTag.title = thisImg.attr(`title`);
+              formattedImgTag.alt = thisImg.attr(`alt`);
 
               if (!formattedImgTag.url) {
-                return resolve()
+                return resolve();
               }
 
-              const fileType = getImageInfo(formattedImgTag.url).ext
+              const fileType = getImageInfo(formattedImgTag.url).ext;
 
               // Ignore gifs as we can't process them,
               // svgs as they are already responsive by definition
@@ -472,26 +477,26 @@ module.exports = (
                   formattedImgTag,
                   resolve,
                   inLink
-                )
+                );
 
                 if (rawHTML) {
                   // Replace the image string
-                  thisImg.replaceWith(rawHTML)
+                  thisImg.replaceWith(rawHTML);
                 } else {
-                  return resolve()
+                  return resolve();
                 }
               }
             }
 
             // Replace the image node with an inline HTML node.
-            node.type = `html`
-            node.value = $(`body`).html() // fix for cheerio v1
+            node.type = `html`;
+            node.value = $(`body`).html(); // fix for cheerio v1
 
-            return resolve(node)
+            return resolve(node);
           })
       )
     ).then(htmlImageNodes =>
       markdownImageNodes.concat(htmlImageNodes).filter(node => !!node)
     )
-  )
-}
+  );
+};
